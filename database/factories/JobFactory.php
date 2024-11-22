79d367df2b203da45adae3c4e7d2f422249f7da5
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-22 12:58:32 +0100

    feat: Added jobs table and requirements

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'description' => $this->faker->paragraph(),
+            'company' => $this->faker->company(),
+            'company_image' => $this->faker->imageUrl($width = 200, $height = 200),
+            'status' => $this->faker->boolean()
+        ];
+    }
+}
