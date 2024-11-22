274ddcf43d4673ab374894a12c3238b2afc44378
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-22 14:08:39 +0100

    feat: Added basic view elements

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable =
+    [
+        'title',
+        'description',
+        'company',
+        'company_image',
+        'status'
+    ];
 }
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header class="header">
+    <h1 class="headerTitle">Job<span>_Search</span></h1>
+    <nav class="headerNav">
+        <a href="">Jobs</a>
+        <a href="">Docs</a>
+    </nav>
+</header>
