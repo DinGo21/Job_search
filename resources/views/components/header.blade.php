96151e361279849f05ff8698d95985d759ae3d82
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-25 10:08:09 +0100

    style: Added css for header and footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header">
     <h1 class="headerTitle">Job<span>_Search</span></h1>
     <nav class="headerNav">
-        <a href="">Jobs</a>
-        <a href="">Docs</a>
+        <a class="headerLink headerLinkActive" href="#">Jobs</a>
+        <a class="headerLink" href="#">Docs</a>
     </nav>
 </header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,8 @@
     <title>@yield('title', 'Job_Search')</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Itim&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/normalizer.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
