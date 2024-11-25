618157f2b84cb8dba483754376abef95bc34e5a6
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-25 17:46:39 +0100

    feat: Added CRUD for api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\JobController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/jobs', [JobController::class, 'index'])->name('apiindex');
+Route::post('/jobs', [JobController::class, 'store'])->name('apistore');
+Route::get('/jobs/{id}', [JobController::class, 'show'])->name('apishow');
+Route::put('/jobs/{id}', [JobController::class, 'update'])->name('apiupdate');
+Route::delete('/jobs/{id}', [JobController::class, 'destroy'])->name('apidestroy');
