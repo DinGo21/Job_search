7f9bd296a4c46b7a24e99869911c3393005f9568
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-26 16:25:20 +0100

    test: Tests for CRUD and Views are successful

---
+++ tests/Feature/JobTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Job;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class JobTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_IndexIsWorking()
+    {
+        $this->withoutExceptionHandling();
+
+        $response = $this->get(route('index'));
+        $response->assertStatus(200)
+                ->assertViewIs('index');
+    }
+
+    public function test_DeleteOneElementFromIndex()
+    {
+        $this->withoutExceptionHandling();
+
+        Job::factory(1)->create();
+        $this->assertDatabaseCount('jobs', 1);
+        $response = $this->get(route('index') . '?action=delete&id=1');
+        $this->assertDatabaseCount('jobs', 0);
+        $response->assertStatus(302);
+    }
+
+    public function test_ShowIsWorking()
+    {
+        $this->withoutExceptionHandling();
+
+        Job::factory(1)->create();
+        $this->assertDatabaseCount('jobs', 1);
+        $response = $this->get(route('show', 1));
+        $response->assertStatus(200)
+                ->assertViewIs('show');
+    }
+}
