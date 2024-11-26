0e086669ad45aa0570c4efaf22a1202249c9ef04
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-26 11:24:34 +0100

    style: Styles for show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,26 +3,32 @@
 @section('content')
     <div class="show">
         <section class="showJob">
-            <img src="{{$job->company_image}}" alt="{{$job->company}}">
-            <h2 class="showJobTitle">{{$job->title}}</h2>
-            <h4 class='showJobCompany'>{{$job->company}}</h4>
-        </section>
-        <section class="showDetails">
-            <p class="showDetailsStatus">
-                status:
-                @if ($job->status === 1)
-                    <span class="jobStatusActive">in progress<span>
-                @else
-                    <span class="jobStatusNotActive">finished</span>
-                @endif
-            </p>
+            <img class="showJobImage" src="{{$job->company_image}}" alt="{{$job->company}}">
+            <div class="showJobContent">
+                <h2 class="showJobTitle">{{$job->title}}</h2>
+                <h4 class='showJobCompany'>{{$job->company}}</h4>
+                <section class="showDetails">
+                    <p class="showDetailsStatus">
+                        <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="#7a7a7a"  
+                            stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline 
+                            icon-tabler-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 
+                            -18 0" /></svg>
+                        status:
+                        @if ($job->status === 1)
+                            <span class="jobStatusActive">in progress<span>
+                        @else
+                            <span class="jobStatusNotActive">finished</span>
+                        @endif
+                    </p>
+                </section>
+            </div>
         </section>
         <section class="showDescription">
             <p class="showDescriptionText">{{$job->description}}</p>
         </section>
         <section class="showOptions">
-            <a class="showGoBack" href="{{route('index')}}">Return</a>
             <a class="showDelete" href="">Delete</a>
+            <a class="showReturn" href="{{route('index')}}">Return</a>
         </section>
     </div>
 @endsection
