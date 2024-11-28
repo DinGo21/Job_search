78febd74dfdc4191f0c40de9582258d6fb329bd7
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-28 11:58:31 +0100

    feat: Added Feedback table and created relation with jobs table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Feedback;
 use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -20,6 +21,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
         Job::factory(5)->create();
+
+        Feedback::factory(10)->create();
     }
 }
